01dfd38dd36ddd7a2c564768b72bca855e2e0a9c
Nikhil <nikhilprakash2012@gmail.com>
2021-05-27 00:20:54 +0530

    26-05-2021-2

--- sublime code/Articulation-point-graph.cpp
+++ sublime code/Articulation-point-graph.cpp
@@ -7,28 +7,33 @@ using namespace std;
 #define pb push_back
 #define For(n) for(int i=0;i<n;i++)
 
-void dfs(int s,int parent,int &timer,vector<int> adj[],vector<int> &intime,vector<int> &low,vector<bool> &vis)
+void dfs(int s,int parent,int &timer,vector<int> adj[],vector<int> &intime,vector<int> &low,vector<bool> &vis,vector<int> &articulation)
 {
    vis[s]=1;
    intime[s]=low[s]=timer++;
-
+   int child=0;
    for(auto a : adj[s])
    {
    	  if(parent==a)continue;
    	  if(!vis[a])
    	  {
-   	  	dfs(a,s,timer,adj,intime,low,vis);
+   	  	dfs(a,s,timer,adj,intime,low,vis,articulation);
    	  	low[s]=min(low[s],low[a]);
    	  	
-   	  	if(low[a]>intime[s])
+   	  	if(low[a]>=intime[s] and parent!=-1)
    	  	{
-   	  		cout<<s<<" "<<a<<endl;
+   	  		articulation[s]=1;
    	  	}
+         child++;
    	  }
    	  else 
    	  {
    	  	low[s]=min(low[s],intime[a]);
    	  }
+   }
+   if(parent==-1 and child>1)
+   {
+      articulation[s]=1;
    }
    
 }
@@ -46,10 +51,15 @@ int main()
    	vector<int> intime(m+5,-1);
    	vector<int> low(m+5,-1);
    	vector<bool> vis(m+5,0);
+      vector<int> articulation(m+5,0);
      int timer=0;
    	for(int i=0;i<n;i++)
    	{
    		if(!vis[i])
-   		dfs(i,-1,timer,adj,intime,low,vis);
+   		dfs(i,-1,timer,adj,intime,low,vis,articulation);
    	}
+      for(int i=0;i<n;i++)
+      {
+         if(articulation[i]==1)cout<<i<<" ";
+      }
 }
